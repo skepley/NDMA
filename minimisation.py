1281083bb6c884e6e9f2a336363971088510a02c
Elena <e.queirolo@vu.nl>
2020-03-31 14:36:12 -0400

    testing on minimization

---
+++ minimisation.py
@@ -0,0 +1,12 @@
+"""
+Tests to learn about pythin and minimization
+
+    Author: Elena Queirolo
+    email: elena.queirolo@rutgers.edu
+"""
+import numpy as np
+from scipy import optimize
+import matplotlib.pyplot as plt
+from math import log
+
+
