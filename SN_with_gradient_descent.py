df2c8fca99950c36be965af665b58163eead49cd
Shane <shane.kepley@rutgers.edu>
2020-05-18 15:35:03 -0400

    Add SaddleNode class and test local minimization functionality

--- SN_with_gradient_descent.py
+++ SN_with_gradient_descent.py
@@ -1,13 +1,5 @@
 """
-One line description of what the script performs (H1 line)
-  
-Optional file header info (to give more details about the function than in the H1 line)
-Optional file header info (to give more details about the function than in the H1 line)
-Optional file header info (to give more details about the function than in the H1 line)
-
-    Output: output
-    Other files required: none
-    See also: OTHER_SCRIPT_NAME,  OTHER_FUNCTION_NAME
+Minimization example with respect to HIll Coefficient for the toggle switch example
    
     Author: Shane Kepley
     email: shane.kepley@rutgers.edu
@@ -16,96 +8,21 @@ Optional file header info (to give more details about the function than in the H
 
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-from scipy.optimize import minimize
-from hill_model import ToggleSwitch, SaddleNode, find_root
+from hill_model import *
+from saddle_node import *
 
-
-def SN_root(SNinstance, parameter, u0):
-    """Return a single root of the SaddleNode rootfinding problem"""
-
-    root = find_root(lambda u: SNinstance.zero_map(u, parameter), lambda u: SNinstance.diff_zero_map(u, parameter), u0,
-                     diagnose=True)
-    return root
-
-
-def SN_call(SNinstance, parameter, n0):
-    """Temporary call method for SaddleNode class"""
-
-    equilibria = SNinstance.model.find_equilibria(10, n0, parameter)
-    initial_eigenvector = np.array([1, -.7])
-    saddleNodePoints = list(filter(lambda root: root.success,
-                                   [SN_root(SNinstance, parameter,
-                                            np.concatenate((equilibria[:, j], initial_eigenvector, n0)))
-                                    for j in
-                                    range(equilibria.shape[1])]))  # return equilibria which converged
-    # saddleNodePoints = list([SN_root(SNinstance, parameter,
-    #                                         np.concatenate((equilibria[:, j], initial_eigenvector, n0)))
-    #                                 for j in
-    #                                 range(equilibria.shape[1])])  # return equilibria which converged
-    hillCoefficients = np.concatenate(([sol.x[-1] for sol in saddleNodePoints], np.array([np.inf])))
-    return np.min(hillCoefficients)
-
-
-def SN_call_grid(SNinstance, parameter):
-
-    # to avoid getting to negative paramter values, we just return infinity if the parameters are biologically not good
-    if np.any(parameter < 0):
-        return np.inf
-
-    # set multiple values of the Hill coefficient and test for the convergence to a saddle node
-    n0_grid = np.linspace(2, 4, 3)
-    list_minima = list(SN_call(SNinstance, parameter, np.array([n])) for n in n0_grid)
-
-    return np.min(np.array(list_minima))
-
-
-def optimizing_parameters(SNinstance, parameter0):
-
-    def local_function(parameter):
-        n = SN_call_grid(SNinstance, parameter)
-        print(n)
-        print(parameter)
-        return n
-
-    best_parameter = minimize(local_function, parameter0, method='nelder-mead', options={'xatol': 1e-2})
-
-     # n_best = local_function(best_parameter)
-
-    return best_parameter #, n_best
-
-
-
-# set some parameters to test using MATLAB toggle switch for ground truth
+# set some parameters
 decay = np.array([1, 1], dtype=float)  # gamma
 p1 = np.array([np.nan, np.nan, np.nan], dtype=float)  # (ell_1, delta_1, theta_1)
 p2 = np.array([np.nan, np.nan, np.nan], dtype=float)  # (ell_2, delta_2, theta_2)
-
 f = ToggleSwitch(decay, [p1, p2])
-f1 = f.coordinates[0]
-f2 = f.coordinates[1]
-H1 = f1.components[0]
-H2 = f2.components[0]
 n0 = 4.1
-
-x0 = np.array([4, 3])
-p0 = np.array([1, 5, 3, 1, 6, 3],
-              dtype=float)  # (gamma_1, ell_1, delta_1, theta_1, gamma_2, ell_2, delta_2, theta_2)
 SN = SaddleNode(f)
 
-# ==== find saddle node for a parameter choice
-p0 = np.array([1, 5, 3, 1, 5, 3], dtype=float)
-SNpoints = SN_call(SN, p0, np.array([4.1]))
-print(SNpoints)
+# ==== find saddle node minimizer for some initial parameter choice
+p0 = np.array([1, 5, 3, 1, 6, 3],
+              dtype=float)  # (gamma_1, ell_1, delta_1, theta_1, gamma_2, ell_2, delta_2, theta_2)
+p1 = np.array([1, 4, 3, 1, 5, 3], dtype=float)
 
-best_par = optimizing_parameters(SN, p0)
-
-print(best_par)
-# v0 = np.array([1, -.7])
-# eq0 = f.find_equilibria(10, n0, p0)
-# x0 = eq0[:, -1]
-# u0 = np.concatenate((x0, v0, np.array(n0)), axis=None)
-# u0Sol = SN_call_temp(SN, p0, u0)
-# # print(u0Sol)
-# x0Sol, v0Sol, n0Sol = [u0Sol.x[idx] for idx in [[0, 1], [2, 3], [4]]]
-# compare to u0Sol = [ 4.55637172,  2.25827744,  0.82199933, -0.56948846,  3.17447061]
+localMinimum = SN.find_minimizer(p1)
+print(localMinimum)
--- hill_model.py
+++ hill_model.py
@@ -6,8 +6,8 @@ Classes and methods for constructing, evaluating, and doing parameter continuati
     Date: 2/29/20; Last revision: 3/4/20
 """
 import numpy as np
-from scipy import optimize
 import matplotlib.pyplot as plt
+from scipy import optimize
 from numpy import log
 
 plt.close('all')
@@ -17,6 +17,25 @@ def isvector(array):
     """Returns true if input is a numpy vector"""
 
     return len(np.shape(array)) == 1
+
+
+def ezcat(*coordinates):
+    """A multiple dispatch concatenation function for numpy arrays. Accepts arbitrary inputs as int, float, tuple,
+    list, or numpy array and concatenates into a vector returned as a numpy array. This is recursive so probably not
+    very efficient for large scale use."""
+
+    if len(coordinates) == 1:
+        if isinstance(coordinates[0], list):
+            return np.array(coordinates[0])
+        elif isinstance(coordinates[0], np.ndarray):
+            return coordinates[0]
+        else:
+            return np.array([coordinates[0]])
+
+    try:
+        return np.concatenate([coordinates[0], ezcat(*coordinates[1:])])
+    except ValueError:
+        return np.concatenate([np.array([coordinates[0]]), ezcat(*coordinates[1:])])
 
 
 class HillComponent:
@@ -68,6 +87,8 @@ class HillComponent:
 
     def __call__(self, x, parameter=np.array([])):
         """Evaluation method for a Hill component function instance"""
+
+        # TODO: Handle the case that negative x values are passed into this function.
 
         ell, delta, theta, hillCoefficient = self.curry_parameters(
             parameter)  # unpack fixed and variable parameter values
@@ -571,7 +592,7 @@ class ToggleSwitch(HillModel):
     def plot_nullcline(self, n, parameter=np.array([]), nNodes=100, domainBounds=(10, 10)):
         """Plot the nullclines for the toggle switch at a given parameter"""
 
-        equilibria = self.find_equilibria(10, n, parameter)
+        equilibria = self.find_equilibria(25, n, parameter)
         Xp = np.linspace(0, domainBounds[0], nNodes)
         Yp = np.linspace(0, domainBounds[1], nNodes)
         Z = np.zeros_like(Xp)
@@ -603,73 +624,7 @@ class ToggleSwitch(HillModel):
 #     return v / np.linalg.norm(v)
 
 
-class SaddleNode:
-    """A instance of a saddle-node bifurcation problem for a HillModel with 1 hill coefficient parameter which is
-    shared by all HillComponents"""
 
-    def __init__(self, hillModel, phaseCondition=lambda v: np.linalg.norm(v) - 1,
-                 phaseConditionDerivative=lambda v: v / np.linalg.norm(v)):
-        """Construct an instance of a saddle-node problem for specified HillModel"""
-
-        self.model = hillModel
-        self.phaseCondition = phaseCondition
-        self.diffPhaseCondition = phaseConditionDerivative
-        self.mapDimension = 2 * hillModel.dimension + 1  # dimension of the zero finding map
-
-    def unpack_components(self, u):
-        """Unpack the input vector for a SaddleNode problem into 3 component vectors of the form (x, v, n)"""
-        return u[0:self.model.dimension], u[self.model.dimension:-1], u[-1]
-
-    def zero_map(self, u, parameter):
-        """A zero finding map for saddle-node bifurcations of the form g: R^{2n+1} ---> R^{2n+1} whose roots are
-        isolated parameters for which a saddle-node bifurcation occurs.
-        INPUT: u = (x, v, hillCoefficient) where x is a state vector, v a tangent vector."""
-
-        stateVector, tangentVector, hillCoefficient = self.unpack_components(u)  # unpack input vector
-        g1 = self.model(stateVector,
-                        hillCoefficient,
-                        parameter)  # this is zero iff x is an equilibrium for the system at parameter value n
-        g2 = self.model.dx(stateVector,
-                           hillCoefficient,
-                           parameter) @ tangentVector  # this is zero iff v lies in the kernel of Df(x, n)
-        g3 = self.phaseCondition(tangentVector)  # this is zero iff v satisfies the phase condition
-        return np.concatenate((g1, g2, g3), axis=None)
-
-    def diff_zero_map(self, u, parameter):
-        """Evaluate the derivative of the zero finding map. This is a matrix valued function of the form
-        Dg: R^{2n+1} ---> M_{2n+1}(R).
-        INPUT: u = (x, v, hillCoefficient) where x is a state vector, v a tangent vector."""
-
-        # unpack input vector and set dimensions for Jacobian blocks
-        stateVector, tangentVector, hillCoefficient = self.unpack_components(u)  # unpack input vector
-        n = self.model.dimension
-        fullParameter = self.model.parse_parameter(hillCoefficient,
-                                                   parameter)  # insert copies of HillCoefficient into parameter vector
-        parameterByCoordinate = self.model.unpack_variable_parameters(
-            fullParameter)  # unpack full parameter vector by coordinate
-        Dg = np.zeros([self.mapDimension, self.mapDimension])  # initialize (2n+1)-by-(2n+1) matrix
-        Df = self.model.dx(stateVector, hillCoefficient,
-                           parameter)  # store derivative of vector field which appears in 2 blocks
-        # TODO: Function calls for derivatives in HIllModel are overloaded to have input syntax (x, n, parm) where as
-        #  the ad hoc Hessian calls use (x, parm). These should be changed to the former.
-
-        # BLOCK ROW 1
-        Dg[0:n, 0:n] = Df  # block - (1,1)
-        # block - (1, 2) is an n-by-n zero block
-        Dg[0:n, -1] = self.model.dn(stateVector, hillCoefficient, parameter)  # block - (1,3)
-        # BLOCK ROW 2
-        Dg[n:2 * n, 0:n] = np.row_stack(
-            [tangentVector @ self.model.coordinates[j].dx2(stateVector, parameterByCoordinate[j])
-             for j in range(n)])  # block - (2,1)
-        Dg[n:2 * n, n:2 * n] = Df  # block - (2,2)
-        Dg[n:2 * n, -1] = np.row_stack(list(
-            map(lambda f_i, parm: f_i.dndx(stateVector, parm), self.model.coordinates,
-                parameterByCoordinate))) @ tangentVector  # block - (2,3)
-        # BLOCK ROW 3
-        # block - (3, 1) is a 1-by-n zero block
-        Dg[-1, n:2 * n] = self.diffPhaseCondition(tangentVector)  # block - (3,2)
-        # block - (3, 1) is a 1-by-1 zero block
-        return Dg
 
 # ## toggle switch plus
 # # set some parameters to test
