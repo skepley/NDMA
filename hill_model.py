Fixed derivative methods for HillModel to use the globalStateIndex instead of interactionIndex of HillCoordinates. All derivatives appear to work properly.

hill_model.py
@@ -81,10 +81,8 @@ def full_newton(f, Df, x0, maxDefect=1e-13):
                 x -= np.linalg.solve(Dy, y)  # update x
 
             y = f(x)  # update f(x)
-            print(y)
             Dy = Df(x)  # update Df(x)
             iDefect = np.linalg.norm(y)  # initialize defect
-            print(iDefect)
             iIterate += 1
 
         if iDefect < maxDefect:
@@ -887,7 +885,6 @@ class HillCoordinate:
             diffInteraction = self.diff_interaction(x,
                                                     parameter,
                                                     1)  # evaluate derivative of interaction function (outer term in chain rule)
-            print(diffInteraction)
             DHillComponent = np.array(
                 list(map(lambda H, x_k, parm: H.dx(x_k, parm), self.components, xLocal,
                          parameterByComponent)))  # evaluate vector of partial derivatives for Hill components (inner term in chain rule)
@@ -1283,12 +1280,8 @@ class HillModel:
         parameterByCoordinate = self.unpack_variable_parameters(parameter)  # unpack variable parameters by component
         for iCoordinate in range(self.dimension):
             f_i = self.coordinates[iCoordinate]  # assign this coordinate function to a variable
-            Dxf[np.ix_([iCoordinate], f_i.interactionIndex)] = f_i.dx(x, parameterByCoordinate[
-                iCoordinate])  # insert derivative of this coordinate
-
+            Dxf[np.ix_([iCoordinate], f_i.globalStateIndex)] = f_i.dx(x, parameterByCoordinate[iCoordinate])  # insert derivative of this coordinate
         return Dxf
-        # return np.vstack(list(map(lambda f_i, parm: f_i.dx(x, parm), self.coordinates,
-        #                           parameterByCoordinate)))  # return a vertical stack of gradient (row) vectors
 
     def diff(self, x, *parameter, diffIndex=None):
         """Return the derivative (Jacobian) of the HillModel vector field with respect to n assuming n is a VECTOR
@@ -1308,7 +1301,6 @@ class HillModel:
                                            self.variableIndexByCoordinate[iCoordinate + 1])
                 Dpf[np.ix_([iCoordinate], parameterSlice)] = f_i.diff(x, parameterByCoordinate[
                     iCoordinate])  # insert derivative of this coordinate
-
             return Dpf
         else:
             raise ValueError  # this isn't implemented yet
@@ -1320,7 +1312,7 @@ class HillModel:
         parameterByCoordinate = self.unpack_variable_parameters(parameter)  # unpack variable parameters by component
         for iCoordinate in range(self.dimension):
             f_i = self.coordinates[iCoordinate]  # assign this coordinate function to a variable
-            xSlice = np.array(f_i.interactionIndex)
+            xSlice = np.array(f_i.globalStateIndex)
             Dxf[np.ix_([iCoordinate], xSlice, xSlice)] = f_i.dx2(x, parameterByCoordinate[
                 iCoordinate])  # insert derivative of this coordinate
         return Dxf
@@ -1336,8 +1328,7 @@ class HillModel:
                 f_i = self.coordinates[iCoordinate]  # assign this coordinate function to a variable
                 parameterSlice = np.arange(self.variableIndexByCoordinate[iCoordinate],
                                            self.variableIndexByCoordinate[iCoordinate + 1])
-                xSlice = np.array(f_i.interactionIndex)
-
+                xSlice = np.array(f_i.globalStateIndex)
                 Dpxf[np.ix_([iCoordinate], xSlice, parameterSlice)] = f_i.dxdiff(x, parameterByCoordinate[
                     iCoordinate])  # insert derivative of this coordinate
             return Dpxf
@@ -1368,7 +1359,7 @@ class HillModel:
         parameterByCoordinate = self.unpack_variable_parameters(parameter)  # unpack variable parameters by component
         for iCoordinate in range(self.dimension):
             f_i = self.coordinates[iCoordinate]  # assign this coordinate function to a variable
-            xSlice = np.array(f_i.interactionIndex)
+            xSlice = np.array(f_i.globalStateIndex)
             Dxxxf[np.ix_([iCoordinate], xSlice, xSlice, xSlice)] = f_i.dx3(x, parameterByCoordinate[
                 iCoordinate])  # insert derivative of this coordinate
         return Dxxxf
@@ -1384,7 +1375,7 @@ class HillModel:
                 f_i = self.coordinates[iCoordinate]  # assign this coordinate function to a variable
                 parameterSlice = np.arange(self.variableIndexByCoordinate[iCoordinate],
                                            self.variableIndexByCoordinate[iCoordinate + 1])
-                xSlice = np.array(f_i.interactionIndex)
+                xSlice = np.array(f_i.globalStateIndex)
                 Dppxf[np.ix_([iCoordinate], xSlice, parameterSlice, parameterSlice)] = f_i.diff2(x,
                                                                                                  parameterByCoordinate[
                                                                                                      iCoordinate])  # insert derivative of this coordinate

test_HillModel.py
@@ -34,6 +34,8 @@ f2 = f.coordinates[1]
 
 x = np.array([4, 3], dtype=float)
 p = ezcat(*[ezcat(*tup) for tup in zip(gamma, componentParmValues)])  # this only works when all parameters are variable
+p1 = p[:5]
+p2 = p[5:]
 
 # H1 = f.components[0]
 # H2 = f.components[1]
@@ -44,30 +46,29 @@ p = ezcat(*[ezcat(*tup) for tup in zip(gamma, componentParmValues)])  # this onl
 
 
 y = f(x, p)
-f1.dx(x,p)
-# yx = f.dx(x,p)
-# yp = f.diff(x,p)
-# yxx = f.dx2(x,p)
+yx = f.dx(x,p)
+yp = f.diff(x,p)
+yxx = f.dx2(x,p)
 
 
 
 
 # test Hill model equilibrium finding
-# eq = f.find_equilibria(10, p)
-# print(eq)
+eq = f.find_equilibria(10, p)
+print(eq)
 # added vectorized evaluation of Hill Models - DONE
 
 
 # plot nullclines and equilibria
-# plt.close('all')
-# Xp = np.linspace(0, 10, 100)
-# Yp = np.linspace(0, 10, 100)
-# Z = np.zeros_like(Xp)
-#
-# N1 = ts.coordinates[0](np.row_stack([Z, Yp])) / gamma[0]  # f1 = 0 nullcline
-# N2 = ts.coordinates[1](np.row_stack([Xp, Z])) / gamma[1]  # f2 = 0 nullcline
-#
-# plt.figure()
-# plt.scatter(eq[0, :], eq[1, :])
-# plt.plot(Xp, N2)
-# plt.plot(N1, Yp)
+plt.close('all')
+Xp = np.linspace(0, 10, 100)
+Yp = np.linspace(0, 10, 100)
+Z = np.zeros_like(Xp)
+
+N1 = f1(np.row_stack([Z, Yp]), p1) / gamma[0]  # f1 = 0 nullcline
+N2 = f2(np.row_stack([Xp, Z]), p2) / gamma[1]  # f2 = 0 nullcline
+
+plt.figure()
+plt.scatter(eq[0, :], eq[1, :])
+plt.plot(Xp, N2)
+plt.plot(N1, Yp)