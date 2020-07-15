diff and diff2 fixed for multiple parameters derivatives SLSQP minimisation doesn't work

hysterisis.py
@@ -50,7 +50,7 @@ def wrapper_minimization(HM, starting_pars, parameterIndex=1, problem='hysteresi
     else:
         raise Exception("Not coded yet = only hysteresis considered")
 
-    results = minimize(min_function, all_starting_values, method='SLSQP', jac='True', constraints=all_constraints)
+    results = minimize(min_function, all_starting_values, method='SLSQP', jac='True', constraints=all_constraints[0])
     return results
 
 
@@ -60,7 +60,7 @@ def negative_distance(parameterIndex):
         gamma1 = variables[parameterIndex + 5]
         fun = gamma0 - gamma1
         dim_jac = np.shape(variables)[0]
-        jac = np.zeros([dim_jac, dim_jac])
+        jac = np.zeros([dim_jac])
         jac[parameterIndex] = 1
         jac[parameterIndex + 5] = -1
         return fun, jac
@@ -69,21 +69,40 @@ def negative_distance(parameterIndex):
 
 def one_saddlenode_problem(SN_loc, first_or_second, paramIndex):
     def saddle_node_problem(variables):
-        fixed_pars = variables[-8:]
-        u_and_v_index0 = 1 + (first_or_second - 1)*5
-        u_and_v = variables[u_and_v_index0:u_and_v_index0 + 4]
-        gamma = variables[u_and_v_index0-1]
+        n = SN_loc.model.dimension
+        num_pars = SN_loc.model.nVariableParameter
+        fixed_pars = variables[-num_pars:]
+        index_gamma = (first_or_second - 1)*(2 * n + 1)
+        u_and_v_index0 = 1 + index_gamma
+        u_and_v = variables[u_and_v_index0:u_and_v_index0 + 2*n]
+        gamma = variables[index_gamma]
         all_vars = ezcat(u_and_v, fixed_pars[0:paramIndex], gamma, fixed_pars[paramIndex:])
+
         return SN_loc(all_vars)
 
     def saddle_node_jac(variables):
-        fixed_pars = variables[-8:]
-        u_and_v_index0 = 1 + (first_or_second - 1)*5
-        u_and_v = variables[u_and_v_index0:u_and_v_index0 + 4]
-        gamma = variables[u_and_v_index0-1]
+        # variables = lambda1, x1, v1, lambda2, x2, v2, other_pars
+        n = SN_loc.model.dimension
+        num_pars = SN_loc.model.nVariableParameter
+        fixed_pars = variables[-num_pars:]
+
+        index_gamma = (first_or_second - 1)*(2 * n + 1)
+        u_and_v_index0 = 1 + index_gamma
+        u_and_v = variables[u_and_v_index0:u_and_v_index0 + 2*n]
+        gamma = variables[index_gamma]
         all_vars = ezcat(u_and_v, fixed_pars[0:paramIndex], gamma, fixed_pars[paramIndex:])
-        # warning: not sure if diff does the trick here
-        return SN_loc.diff(all_vars)
+        non_zero_diff = SN_loc.diff(all_vars)
+
+        # reordering of the result w.r.t. the global ordering
+        full_derivative = np.zeros([non_zero_diff.shape[0], len(variables)])
+        #
+        full_derivative[:, index_gamma] = non_zero_diff[:, 2*n + paramIndex]
+        full_derivative[:, index_gamma+1: index_gamma+2 * n + 1] = non_zero_diff[:, 0:2*n]
+
+        indices_fixed_pars = np.arange(num_pars)
+        indices_fixed_pars = np.delete(indices_fixed_pars, paramIndex)
+        full_derivative[:, -num_pars+1:] = non_zero_diff[:, 2*n+indices_fixed_pars]
+        return full_derivative
     return saddle_node_problem, saddle_node_jac
 
 
@@ -107,7 +126,7 @@ def parameter_norm():
         return np.sum(parameter)
 
     def jac_norm_par(parameters):
-        jac = np.ones_like(parameters)
+        jac = np.ones(len(parameters))
         return jac
     return norm_par, jac_norm_par
 
@@ -172,8 +191,8 @@ print('Distance = ', distance)
 
 
 long_p = np.append([rho], p)
-wrapper_minimization(f, long_p)
-
+results = wrapper_minimization(f, long_p)
+print(results)
 #print('Minimal distance = ', res)
 
 

saddle_node.py
@@ -129,23 +129,40 @@ class SaddleNode:
         index1 = np.arange(n)
         index2 = np.arange(n) + n
         index3 = 2 * n + np.arange(parameterDim)
-
-        # BLOCK ROW 1
-        Dg[np.ix_(index1, index1)] = Dxf  # block - (1,1)
-        # block - (1, 2) is an n-by-n zero block
-        a = self.model.diff(stateVector, fullParameter, diffIndex=diffIndex)  # block - (1,3)
-        Dg[np.ix_(index1, index3)] = a
-        # BLOCK ROW 2
-        Dg[np.ix_(index2, index1)] = np.einsum('ijk,j', self.model.dx2(stateVector, fullParameter),
-                                       tangentVector)  # block - (2,1)
-        Dg[np.ix_(index2, index2)] = Dxf  # block - (2,2)
-        Dg[np.ix_(index2, index3)] = np.einsum('ij, j',
-                                       self.model.dxdiff(stateVector, fullParameter, diffIndex=diffIndex),
-                                       tangentVector)  # block - (2,3)
-        # BLOCK ROW 3
-        # block - (3, 1) is a 1-by-n zero block
-        Dg[np.ix_(index3, index2)] = self.diffPhaseCondition(tangentVector)  # block - (3,2)
-        # block - (3, 1) is a 1-by-1 zero block
+        if parameterDim is 1:
+            # BLOCK ROW 1
+            Dg[np.ix_(index1, index1)] = Dxf  # block - (1,1)
+            # block - (1, 2) is an n-by-n zero block
+            a = self.model.diff(stateVector, fullParameter, diffIndex=diffIndex)  # block - (1,3)
+            Dg[index1, index3] = a
+            # BLOCK ROW 2
+            Dg[np.ix_(index2, index1)] = np.einsum('ijk,j', self.model.dx2(stateVector, fullParameter),
+                                           tangentVector)  # block - (2,1)
+            Dg[np.ix_(index2, index2)] = Dxf  # block - (2,2)
+            Dg[index2, index3] = np.einsum('ij, j',
+                                           self.model.dxdiff(stateVector, fullParameter, diffIndex=diffIndex),
+                                           tangentVector)  # block - (2,3)
+            # BLOCK ROW 3
+            # block - (3, 1) is a 1-by-n zero block
+            Dg[index3, np.ix_(index2)] = self.diffPhaseCondition(tangentVector)  # block - (3,2)
+            # block - (3, 1) is a 1-by-1 zero block
+        else:
+            # BLOCK ROW 1
+            Dg[np.ix_(index1, index1)] = Dxf  # block - (1,1)
+            # block - (1, 2) is an n-by-n zero block
+            a = self.model.diff(stateVector, fullParameter, diffIndex=diffIndex)  # block - (1,3)
+            Dg[np.ix_(index1, index3)] = a
+            # BLOCK ROW 2
+            Dg[np.ix_(index2, index1)] = np.einsum('ijk,j', self.model.dx2(stateVector, fullParameter),
+                                           tangentVector)  # block - (2,1)
+            Dg[np.ix_(index2, index2)] = Dxf  # block - (2,2)
+            Dg[np.ix_(index2, index3)] = np.einsum('ijk, j',
+                                           self.model.dxdiff(stateVector, fullParameter, diffIndex=diffIndex),
+                                           tangentVector)  # block - (2,3)
+            # BLOCK ROW 3
+            # block - (3, 1) is a 1-by-n zero block
+            Dg[np.ix_(index3, index2)] = self.diffPhaseCondition(tangentVector)  # block - (3,2)
+            # block - (3, 1) is a 1-by-1 zero block
         return Dg
 
     def diff2(self, u, diffIndex=None):
@@ -171,26 +188,45 @@ class SaddleNode:
         index1 = np.array(range(n))
         index2 = np.array(range(n)) + n
         index3 = 2 * n + np.array(range(parameterDim))
+        if parameterDim is 1:
+            # ROW 1
+            Dg[np.ix_(index1, index1, index1)] = Dxxf  # block - (1,1,1)
+            Dg[np.ix_(index1, index1), index3] = Dxpf
+            Dg[np.ix_(index1), index3, np.ix_(index1)] = Dxpf
+            Dg[np.ix_(index1), index3, index3] = Dppf
 
-        # ROW 1
-        Dg[np.ix_(index1, index1, index1)] = Dxxf  # block - (1,1,1)
-        Dg[np.ix_(index1, index1, index3)] = Dxpf
-        Dg[np.ix_(index1, index3, index1)] = Dxpf
-        Dg[np.ix_(index1, index3, index3)] = Dppf
+            # BLOCK ROW 2 - derivatives of Dxf*v
+            Dg[np.ix_(index2, index1, index1)] = np.einsum('ijkl,j', Dxxxf, tangentVector)
+            Dg[np.ix_(index2, index1, index2)] = Dxxf
+            Dg[np.ix_(index2, index1), index3] = np.einsum('ijk,j', Dxxpf, tangentVector)
+            Dg[np.ix_(index2, index2, index1)] = Dxxf
+            Dg[np.ix_(index2, index2), index3] = Dxpf
+            Dg[np.ix_(index2), index3, np.ix_(index1)] = np.einsum('ijk,j', Dxxpf, tangentVector)
+            Dg[np.ix_(index2), index3, np.ix_(index2)] = Dxpf
+            Dg[np.ix_(index2), index3, index3] = Dxppf
 
-        # BLOCK ROW 2 - derivatives of Dxf*v
-        Dg[np.ix_(index2, index1, index1)] = np.einsum('ijkl,j', Dxxxf, tangentVector)
-        Dg[np.ix_(index2, index1, index2)] = Dxxf
-        Dg[np.ix_(index2, index1, index3)] = np.einsum('ijk,j', Dxxpf, tangentVector)
-        Dg[np.ix_(index2, index2, index1)] = Dxxf
-        Dg[np.ix_(index2, index2, index3)] = Dxpf
-        Dg[np.ix_(index2, index3, index1)] = np.einsum('ijk,j', Dxxpf, tangentVector)
-        Dg[np.ix_(index2, index3, index2)] = Dxpf
-        Dg[np.ix_(index2, index3, index3)] = Dxppf
+            # BLOCK ROW 3
+            # block - (3, :, :) is a 1-by-dim-by-dim zero block because the phase condition is linear
+        else:
 
-        # BLOCK ROW 3
-        # block - (3, :, :) is a 1-by-dim-by-dim zero block because the phase condition is linear
+            # ROW 1
+            Dg[np.ix_(index1, index1, index1)] = Dxxf  # block - (1,1,1)
+            Dg[np.ix_(index1, index1, index3)] = Dxpf
+            Dg[np.ix_(index1, index3, index1)] = Dxpf
+            Dg[np.ix_(index1, index3, index3)] = Dppf
 
+            # BLOCK ROW 2 - derivatives of Dxf*v
+            Dg[np.ix_(index2, index1, index1)] = np.einsum('ijkl,j', Dxxxf, tangentVector)
+            Dg[np.ix_(index2, index1, index2)] = Dxxf
+            Dg[np.ix_(index2, index1, index3)] = np.einsum('ijk,j', Dxxpf, tangentVector)
+            Dg[np.ix_(index2, index2, index1)] = Dxxf
+            Dg[np.ix_(index2, index2, index3)] = Dxpf
+            Dg[np.ix_(index2, index3, index1)] = np.einsum('ijk,j', Dxxpf, tangentVector)
+            Dg[np.ix_(index2, index3, index2)] = Dxpf
+            Dg[np.ix_(index2, index3, index3)] = Dxppf
+
+            # BLOCK ROW 3
+            # block - (3, :, :) is a 1-by-dim-by-dim zero block because the phase condition is linear
         return Dg
 
     def global_jac(self, par, u):