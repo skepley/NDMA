b4654c4f19da641d2ef27b7b3df615f9bc39a25c
Elena <e.queirolo@vu.nl>
2021-01-15 12:48:44 -0500

    general fiber sampling in toggle_switch_heat_functionalities
    inputs a 2D DSGRN coordinate, returns a random sample on the fiber - not tested

--- toggle_switch_heat_functionalities.py
+++ toggle_switch_heat_functionalities.py
@@ -1,5 +1,6 @@
 from hill_model import *
 
+
 def sampler():
     """Sample parameters for the toggle switch other than the hill coefficient. This is a nondimensionalized sampler
      so it assumes that theta_1 = theta_2 = gamma_1 = 1 and returns a vector in R^5 of the form:
@@ -21,6 +22,38 @@ def sampler():
     delta_2 = deltaByGamma_2 * gamma_2
     return ezcat(ell_1, delta_1, gamma_2, ell_2, delta_2)
 
+
+def fiber_sampler(u, v, alpha_bar=10):
+    """Samples the fiber defined by (u,v) according to the formulas presented in the -temporary- Section 4.4.1"""
+
+    # the first component: u, relates to gamma_2, ell_2, delta_2
+    if u < 1:
+        ell_2 = np.random.random_sample()
+        delta_2 = np.random.random_sample()
+        gamma_2 = u / (ell_2 + delta_2)
+    elif u < 2:
+        ell_2 = np.random.random_sample()
+        gamma_2 = ell_2 + np.random.random_sample()
+        delta_2 = (gamma_2 - ell_2) / (u - 1)
+    else:
+        ell_2 = np.random.random_sample()
+        delta_2 = np.random.random_sample()
+        gamma_2 = (u - 2) * (alpha_bar - 1) + 1
+
+    # the second component: v, relates to ell_1, delta_1
+    if v < 1:
+        ell_1 = v * np.random.random_sample()  # sample in (0, v)
+        delta_1 = v - ell_1
+    elif v < 2:
+        ell_1 = np.random.random_sample()
+        delta_1 = (1 - ell_1) / (v - 1)
+    else:
+        ell_1 = (v - 2) * (alpha_bar - 1) + 1
+        delta_1 = np.random.random_sample()
+
+    return ezcat(ell_1, delta_1, gamma_2, ell_2, delta_2)
+
+
 def heat_coordinate(alpha, beta, alphaMax):
     """Returns the DSGRN heat map coordinates For a parameter of the form (alpha, beta) where
     alpha = ell / gamma and beta = (ell + delta) / gamma"""
@@ -34,6 +67,7 @@ def heat_coordinate(alpha, beta, alphaMax):
     else:  # ell/gamma < theta < (ell + delta)/gamma
         x = 1 + (1 - alpha) / (beta - alpha)
     return x
+
 
 def heat_coordinates(alpha1, beta1, alpha2, beta2, alphaMax):
     """ take vector of coordinates and return vectors of x-coordinates and y-ccordinates"""
@@ -64,10 +98,14 @@ def dsgrn_plot(parameterArray, alphaMax=None):
     if alphaMax is None:
         alphaMax = np.max(np.max(alpha1), np.max(alpha2))
 
-    x, y =  heat_coordinates(alpha1, beta1, alpha2, beta2, alphaMax)
+    x, y = heat_coordinates(alpha1, beta1, alpha2, beta2, alphaMax)
 
     plt.scatter(x, y, marker='o', c='k', s=2)
     grid_lines()
 
 
-
+print(fiber_sampler(1.2, 2.3))
+print(fiber_sampler(1.2, 2.3))
+print(fiber_sampler(1.2, 2.3))
+print(fiber_sampler(1.2, 2.3))
+print(fiber_sampler(1.2, 2.3))
