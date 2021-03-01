6e8a263ff19d7e471facb6dcd14fc6c4d3c01c6f
Elena <e.queirolo@vu.nl>
2021-03-01 12:03:16 -0500

    nicer printing

--- test_heat_map.py
+++ test_heat_map.py
@@ -12,7 +12,7 @@ f = ToggleSwitch(decay, [p1, p2])
 SN = SaddleNode(f)
 
 # size of the sample
-n_sample = 30
+n_sample = 300
 # a random parameter list
 u = 1 + np.random.uniform(-0.1, 1.1, n_sample)
 v = 1 + np.random.uniform(-0.1, 1.1, n_sample)
@@ -25,7 +25,6 @@ bad_candidates = []
 boring_parameters = np.empty(shape=[0, 5])
 multiple_saddles = np.empty(shape=[0, 5])
 for j in range(n_sample):
-    b='Completion: '+str(j)+' out of '+str(n_sample)
     a_j = a[j, :]
     SNParameters, badCandidates = find_saddle_coef(f, [1, 50], a_j)
     if SNParameters and SNParameters is not 0:
@@ -40,13 +39,12 @@ for j in range(n_sample):
                 print('More than one saddle detected!')
                 multiple_saddles = np.append(multiple_saddles, [a_j], axis=0)
     if badCandidates and badCandidates is not 0:
-        print('A bad parameter\n')
+        print('\nA bad parameter')
         bad_parameters = np.append(bad_parameters, [a_j], axis=0)
         bad_candidates.append(badCandidates)
-        print(b)
-    else:
-        sys.stdout.write('\r' + b)
-        sys.stdout.flush()
+    printing_statement = 'Completion: ' + str(j) + ' out of ' + str(n_sample)
+    sys.stdout.write('\r' + printing_statement)
+    sys.stdout.flush()
 
     if SNParameters is 0 and badCandidates is 0:
         #print('boooring')
@@ -60,7 +58,7 @@ np.savez('data_center_region_small',
          bad_candidates=bad_candidates, boring_parameters=boring_parameters)
 np.load('data_center_region_small.npz')
 
-print('Number of bad candidates', len(bad_candidates), 'out of ', n_sample)
+print('\nNumber of bad candidates', len(bad_candidates), 'out of ', n_sample)
 print('Number of boring candidates', len(boring_parameters), 'out of ', n_sample)
 print('Number of saddles', len(parameter_full), 'out of ', n_sample)
 
