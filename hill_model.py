Fixed bugs in HillModel derivatives, dx, dx2, dx3, and dxdiff. The problem is that both HillModel and HillCoordinate were trying to handle global index slicing for the state variables. HillCoordinate derivatives were already padding zeros for missing indices so the derivatives were full size tensors which HillModel derivatives were trying to pass as slices into a full sized tensor. dx2diff and dxdiff2 are still wrong and need to be fixed in the same way.

hill_model.py
@@ -1179,6 +1179,7 @@ class HillModel:
                                            interactionType[j], [j] + interactionIndex[j], gamma=gamma[j]) for j in
                             range(self.dimension)]
 
+        # TODO: FIX THIS HACKY NONSENSE.
         # This is a hacky and temporary fix to the problem where Hill Components do not know their own dimension based only on
         # the input to their class constructor. This needs to be fixed with HillModel handling all global index bookkeeping.
         for f_i in self.coordinates:
@@ -1232,12 +1233,13 @@ class HillModel:
         NOTE: This function is not vectorized. It assumes x is a single vector in R^n."""
 
         parameter = self.parse_parameter(*parameter)  # concatenate all parameters into a vector
-        Dxf = np.zeros(2 * [self.dimension])  # initialize Derivative as 2-tensor (Jacobian matrix)
         parameterByCoordinate = self.unpack_variable_parameters(parameter)  # unpack variable parameters by component
-        for iCoordinate in range(self.dimension):
-            f_i = self.coordinates[iCoordinate]  # assign this coordinate function to a variable
-            Dxf[np.ix_([iCoordinate], f_i.globalStateIndex)] = f_i.dx(x, parameterByCoordinate[
-                iCoordinate])  # insert derivative of this coordinate
+        Dxf = np.array([self.coordinates[i].dx(x, parameterByCoordinate[i]) for i in range(self.dimension)])
+        # Dxf = np.zeros(2 * [self.dimension])  # initialize Derivative as 2-tensor (Jacobian matrix)
+        # for iCoordinate in range(self.dimension):
+        #     f_i = self.coordinates[iCoordinate]  # assign this coordinate function to a variable
+        #     Dxf[np.ix_([iCoordinate], f_i.globalStateIndex)] = f_i.dx(x, parameterByCoordinate[
+        #             iCoordinate])  # insert derivative of this coordinate
         return Dxf
 
     def diff(self, x, *parameter, diffIndex=None):
@@ -1265,29 +1267,33 @@ class HillModel:
     def dx2(self, x, *parameter):
         """Evaluate the second derivative of f w.r.t. state variable vector (twice), returns a 3D tensr"""
         parameter = self.parse_parameter(*parameter)  # concatenate all parameters into a vector
-        Dxf = np.zeros(3 * [self.dimension])  # initialize Derivative as 3-tensor
         parameterByCoordinate = self.unpack_variable_parameters(parameter)  # unpack variable parameters by component
-        for iCoordinate in range(self.dimension):
-            f_i = self.coordinates[iCoordinate]  # assign this coordinate function to a variable
-            xSlice = np.array(f_i.globalStateIndex)
-            Dxf[np.ix_([iCoordinate], xSlice, xSlice)] = f_i.dx2(x, parameterByCoordinate[
-                iCoordinate])  # insert derivative of this coordinate
+        Dxf = np.array([self.coordinates[i].dx2(x, parameterByCoordinate[i]) for i in range(self.dimension)])
+
+        # Dxf = np.zeros(3 * [self.dimension])  # initialize Derivative as 3-tensor
+        # for iCoordinate in range(self.dimension):
+        #     f_i = self.coordinates[iCoordinate]  # assign this coordinate function to a variable
+        #     xSlice = np.array(f_i.globalStateIndex)
+        #     Dxf[np.ix_([iCoordinate], xSlice, xSlice)] = f_i.dx2(x, parameterByCoordinate[
+        #         iCoordinate])  # insert derivative of this coordinate
         return Dxf
 
     def dxdiff(self, x, *parameter, diffIndex=None):
         """Evaluate the second order mixed derivative of f w.r.t. state variables and parameters (once each)"""
         if diffIndex is None:  # return the full derivative wrt all parameters
             parameter = self.parse_parameter(*parameter)  # concatenate all parameters into a vector
-            Dpxf = np.zeros(2 * [self.dimension] + [len(parameter)])  # initialize Derivative as 3-tensor
             parameterByCoordinate = self.unpack_variable_parameters(
                 parameter)  # unpack variable parameters by component
-            for iCoordinate in range(self.dimension):
-                f_i = self.coordinates[iCoordinate]  # assign this coordinate function to a variable
-                parameterSlice = np.arange(self.variableIndexByCoordinate[iCoordinate],
-                                           self.variableIndexByCoordinate[iCoordinate + 1])
-                xSlice = np.array(f_i.globalStateIndex)
-                Dpxf[np.ix_([iCoordinate], xSlice, parameterSlice)] = f_i.dxdiff(x, parameterByCoordinate[
-                    iCoordinate])  # insert derivative of this coordinate
+            Dpxf = np.array([self.coordinates[i].dxdiff(x, parameterByCoordinate[i]) for i in range(self.dimension)])
+
+            # Dpxf = np.zeros(2 * [self.dimension] + [len(parameter)])  # initialize Derivative as 3-tensor
+            # for iCoordinate in range(self.dimension):
+            #     f_i = self.coordinates[iCoordinate]  # assign this coordinate function to a variable
+            #     parameterSlice = np.arange(self.variableIndexByCoordinate[iCoordinate],
+            #                                self.variableIndexByCoordinate[iCoordinate + 1])
+            #     xSlice = np.array(f_i.globalStateIndex)
+            #     Dpxf[np.ix_([iCoordinate], xSlice, parameterSlice)] = f_i.dxdiff(x, parameterByCoordinate[
+            #         iCoordinate])  # insert derivative of this coordinate
             return Dpxf
         else:
             raise ValueError  # this isn't implemented yet
@@ -1312,13 +1318,14 @@ class HillModel:
     def dx3(self, x, *parameter):
         """Evaluate the third derivative of f w.r.t. state variable vector (three times)"""
         parameter = self.parse_parameter(*parameter)  # concatenate all parameters into a vector
-        Dxxxf = np.zeros(4 * [self.dimension])  # initialize Derivative as 4-tensor
         parameterByCoordinate = self.unpack_variable_parameters(parameter)  # unpack variable parameters by component
-        for iCoordinate in range(self.dimension):
-            f_i = self.coordinates[iCoordinate]  # assign this coordinate function to a variable
-            xSlice = np.array(f_i.globalStateIndex)
-            Dxxxf[np.ix_([iCoordinate], xSlice, xSlice, xSlice)] = f_i.dx3(x, parameterByCoordinate[
-                iCoordinate])  # insert derivative of this coordinate
+        Dxxxf = np.array([self.coordinates[i].dx3(x, parameterByCoordinate[i]) for i in range(self.dimension)])
+        # Dxxxf = np.zeros(4 * [self.dimension])  # initialize Derivative as 4-tensor
+        # for iCoordinate in range(self.dimension):
+        #     f_i = self.coordinates[iCoordinate]  # assign this coordinate function to a variable
+        #     xSlice = np.array(f_i.globalStateIndex)
+        #     Dxxxf[np.ix_([iCoordinate], xSlice, xSlice, xSlice)] = f_i.dx3(x, parameterByCoordinate[
+        #         iCoordinate])  # insert derivative of this coordinate
         return Dxxxf
 
     def dx2diff(self, x, *parameter, diffIndex=None):

models/Network12.py
@@ -34,13 +34,13 @@ class Network12(HillModel):
 
         parameter = [insert_nan(parmArray) for parmArray in parameter]
 
-        # Add interactions for the basic toggle switch
+        # Add interactions defined by the topology and a choice of algebra.
         interactionSigns = [[1, 1, 1], [1, 1], [1]]  # all interactions are activation
         interactionTypes = [[3], [2], [1]]  # all interactions are single summand
         interactionIndex = [[0, 1, 2], [0, 2], [0]]
 
         super().__init__(gamma, parameter, interactionSigns, interactionTypes,
-                         interactionIndex)  # define HillModel for toggle switch by inheritance
+                         interactionIndex)  # define HillModel by inheritance
 
         self.nComponent = np.sum(
             [self.coordinates[j].nComponent for j in range(self.dimension)])  # count total number of Hill components

models/repressilator.py
@@ -43,7 +43,7 @@ class Repressilator(HillModel):
         elif variant == 1:
             interactionSigns = [[-1], [1], [1]]  # 1 repression and 2 activation (weak negative feedback)
 
-        interactionTypes = [[3], [2], [1]]  # all nonlinear interactions depend only on a single gene
+        interactionTypes = [[1], [1], [1]]  # all nonlinear interactions depend only on a single gene
         interactionIndex = [[1], [2], [0]]  # interactions defined by a single cycle
 
         super().__init__(gamma, parameter, interactionSigns, interactionTypes,
@@ -188,23 +188,34 @@ if __name__ == '__main__':
     # Check this against the true values:
 
     print(f(x, hill, p))
+
+
     # TEST DERIVATIVE EVALUATION
     [f0, f1, f2] = f.coordinates
     pT = np.array([3, 100, x[0], 2])
-    p0 = ezcat(3, pT)
-    p1 = ezcat(3, pT)
-    p2 = ezcat(3, pT)
+    p0 = ezcat(gammaValues[0], parmValues[0], hill)
+    p1 = ezcat(gammaValues[1], parmValues[1], hill)
+    p2 = ezcat(gammaValues[2], parmValues[2], hill)
 
-    gamma, p2ByComponent = f2.parse_parameters(p2)
-    Df = np.zeros(f2.dim, dtype=float)
-    xLocal = x[
-        f2.interactionIndex]  # extract only the coordinates of x that this HillCoordinate depends on as a vector in R^{n_i}
-    diffInteraction = f2.diff_interaction(x,
-                                          p2,
-                                          1)  # evaluate derivative of interaction function (outer term in chain rule)
-    DHillComponent = np.array(
-        list(map(lambda H, x_k, parm: H.dx(x_k, parm), f2.components, xLocal,
-                 p2ByComponent)))  # evaluate vector of partial derivatives for Hill components (inner term in chain rule)
-    Df[
-        f2.interactionIndex] = diffInteraction * DHillComponent  # evaluate gradient of nonlinear part via chain rule
-    Df[f2.index] -= gamma  # Add derivative of linear part to the gradient at this HillCoordinate
+    # print(f0.dx(x, p0))
+    # print(f1.dx(x, p1))
+    # print(f2.dx(x, p2))
+    # print(f.dx(x, hill, p))
+
+    y0 = f0.dx2(x, p0)
+    y = f.dx2(x, hill, p)
+
+#
+    # gamma, p2ByComponent = f2.parse_parameters(p2)
+    # Df = np.zeros(f2.dim, dtype=float)
+    # xLocal = x[
+    #     f2.interactionIndex]  # extract only the coordinates of x that this HillCoordinate depends on as a vector in R^{n_i}
+    # diffInteraction = f2.diff_interaction(x,
+    #                                       p2,
+    #                                       1)  # evaluate derivative of interaction function (outer term in chain rule)
+    # DHillComponent = np.array(
+    #     list(map(lambda H, x_k, parm: H.dx(x_k, parm), f2.components, xLocal,
+    #              p2ByComponent)))  # evaluate vector of partial derivatives for Hill components (inner term in chain rule)
+    # Df[
+    #     f2.interactionIndex] = diffInteraction * DHillComponent  # evaluate gradient of nonlinear part via chain rule
+    # Df[f2.index] -= gamma  # Add derivative of linear part to the gradient at this HillCoordinate