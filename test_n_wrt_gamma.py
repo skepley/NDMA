edc0a313125430756c2cfedbcc0934f4c826063d
Elena <elenaquei@hotmail.it>
2021-03-31 14:41:53 -0400

    fixed n wrt gamma - now works as expected

--- test_n_wrt_gamma.py
+++ test_n_wrt_gamma.py
@@ -12,12 +12,13 @@ f = ToggleSwitch(decay, [p1, p2])
 SN = SaddleNode(f)
 
 # size of the sample
-n_sample = 25
+n_sample = 51
 n_second_sample = 25
 # a random parameter list
-u = np.linspace(0.5, 2.5, n_sample)#1 + np.random.uniform(-0.1, 1.1, n_sample)
-v = np.full(n_sample, 1.5)
-a = np.array([fiber_sampler(u[j], v[j]) for j in range(n_sample) for k in range(n_second_sample)])
+u_short = np.linspace(0.5, 2.5, n_sample)#1 + np.random.uniform(-0.1, 1.1, n_sample)
+u = np.repeat(u_short, n_second_sample)
+v = np.full(n_sample*n_second_sample, 1.5)
+a = np.array([fiber_sampler(u[j], v[j]) for j in range(n_sample*n_second_sample)])
 
 parameter_full = np.empty(shape=[0, 5])
 solutions = None
@@ -29,14 +30,15 @@ boring_y = []
 multiple_saddles = np.empty(shape=[0, 5])
 u_info_n_vs_gamma = np.empty(0)
 n_info_n_vs_gamma = np.empty(0)
+
 for j in range(n_sample * n_second_sample):
     a_j = a[j, :]
     SNParameters, badCandidates = find_saddle_coef(f, [1, 50], a_j)
     if SNParameters and SNParameters is not 0:
         for k in range(len(SNParameters)):
             #print('Saddle detected')
-            plt.plot(u[np.mod(j, n_sample)], SNParameters[k], 'ro')
-            u_info_n_vs_gamma = np.append(u_info_n_vs_gamma, u[np.mod(j, n_sample)])
+
+            u_info_n_vs_gamma = np.append(u_info_n_vs_gamma, u[j])
             n_info_n_vs_gamma = np.append(n_info_n_vs_gamma, SNParameters[k])
             parameter_full = np.append(parameter_full, [a_j], axis=0)
             if solutions is None:
@@ -60,38 +62,42 @@ for j in range(n_sample * n_second_sample):
         boring_parameters = np.append(boring_parameters, [a_j], axis=0)
         boring_y.append(u[np.mod(j, n_sample)])
 print('\n')
-plt.title('Hill coef VS x')
-
 
 average_hill = np.empty(n_sample)
 for i in range(n_sample):
-    average_hill[i] = np.mean(n_info_n_vs_gamma[np.where(u_info_n_vs_gamma == u[i])])
-plt.figure()
-plt.plot(u, average_hill, 'o')
-plt.title('Average Hill coefficient')
+    average_hill[i] = np.mean(n_info_n_vs_gamma[np.where(u_info_n_vs_gamma == u_short[i])])
+
 
 count_hill = np.empty(n_sample)
 number_saddles = []
 for i in range(n_sample):
-    count_hill[i] = np.shape(n_info_n_vs_gamma[np.where(u_info_n_vs_gamma == u[i])])[0]
+    count_hill[i] = np.shape(n_info_n_vs_gamma[np.where(u_info_n_vs_gamma == u_short[i])])[0]
     #number_saddles.append(len(count_hill[i]))
 
+np.savez('data_n_wrt_gamma',
+         u=u, v=v, a=a, u_info_n_vs_gamma=u_info_n_vs_gamma, n_info_n_vs_gamma=n_info_n_vs_gamma, average_hill=average_hill,
+         count_hill=count_hill)
+data = np.load('data_n_wrt_gamma.npz')
+
 plt.figure()
-plt.plot(u, count_hill, 'o')
+plt.title('Hill coef VS x')
+plt.plot(data.f.u_info_n_vs_gamma, data.f.n_info_n_vs_gamma, 'ro')
+
+
+plt.figure()
+plt.plot(np.unique(data.f.u), data.f.average_hill, 'o')
+plt.title('Average Hill coefficient')
+
+plt.figure()
+plt.plot(np.unique(data.f.u), data.f.count_hill, 'o')
 plt.title('Number of saddles out of sample number ' + str(n_second_sample))
 
+
+#print('\nNumber of bad candidates', len(bad_candidates), 'out of ', n_sample)
+#print('Number of boring candidates', len(boring_parameters), 'out of ', n_sample)
+#print('Number of saddles', len(parameter_full), 'out of ', n_sample)
+
 """
-np.savez('data_center_region_small',
-         u=u, v=v, a=a, parameter_full=parameter_full, solutions=solutions, bad_parameters=bad_parameters,
-         bad_candidates=bad_candidates, boring_parameters=boring_parameters)
-np.load('data_center_region_small.npz')
-"""
-
-print('\nNumber of bad candidates', len(bad_candidates), 'out of ', n_sample)
-print('Number of boring candidates', len(boring_parameters), 'out of ', n_sample)
-print('Number of saddles', len(parameter_full), 'out of ', n_sample)
-
-
 if bad_parameters is not None:
     fig1 = plt.figure()
     dsgrn_plot(bad_parameters, 10)
@@ -103,5 +109,5 @@ if boring_parameters is not None:
     plt.title('No saddle detected')
 fig1 = plt.figure()
 dsgrn_heat_plot(parameter_full, solutions, 10)
-
+"""
 print('It is the end!')
