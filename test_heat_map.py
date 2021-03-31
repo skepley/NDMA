886cc71189ebab8d9caf086bfede93008df06522
Elena <elenaquei@hotmail.it>
2021-03-31 14:42:04 -0400

    added averaging and contour plots

--- test_heat_map.py
+++ test_heat_map.py
@@ -12,29 +12,33 @@ f = ToggleSwitch(decay, [p1, p2])
 SN = SaddleNode(f)
 
 # size of the sample
-n_sample = 300
+n_sample_side = 10
+n_sample = n_sample_side**2
+n_second_sample = 10
 # a random parameter list
-u = 1 + np.random.uniform(-0.1, 1.1, n_sample)
-v = 1 + np.random.uniform(-0.1, 1.1, n_sample)
-a = np.array([fiber_sampler(u[j], v[j]) for j in range(n_sample)])
+[u, v] = np.meshgrid(np.linspace(0.8, 2.2, n_sample_side), np.linspace(0.8, 2.2, n_sample_side))
+u = u.flatten()
+u = np.repeat(u, n_second_sample)
+v = v.flatten()
+v = np.repeat(v, n_second_sample)
+a = np.array([fiber_sampler(u[j], v[j]) for j in range(n_sample*n_second_sample)])
+
+n_sample = n_sample * n_second_sample
 
 parameter_full = np.empty(shape=[0, 5])
-solutions = None
+solutions = np.empty(0)
 bad_parameters = np.empty(shape=[0, 5])
 bad_candidates = []
 boring_parameters = np.empty(shape=[0, 5])
 multiple_saddles = np.empty(shape=[0, 5])
-for j in range(n_sample):
+for j in range(n_sample):#range(n_sample):
     a_j = a[j, :]
     SNParameters, badCandidates = find_saddle_coef(f, [1, 50], a_j)
     if SNParameters and SNParameters is not 0:
         for k in range(len(SNParameters)):
             #print('Saddle detected')
             parameter_full = np.append(parameter_full, [a_j], axis=0)
-            if solutions is None:
-                solutions = SNParameters[k]
-            else:
-                solutions = ezcat(solutions, SNParameters[k])
+            solutions = np.append(solutions, SNParameters[k])
             if k > 0:
                 print('More than one saddle detected!')
                 multiple_saddles = np.append(multiple_saddles, [a_j], axis=0)
@@ -50,18 +54,14 @@ for j in range(n_sample):
         #print('boooring')
         boring_parameters = np.append(boring_parameters, [a_j], axis=0)
 
-
-
-
-np.savez('data_center_region_small',
+np.savez('averaging_data',
          u=u, v=v, a=a, parameter_full=parameter_full, solutions=solutions, bad_parameters=bad_parameters,
          bad_candidates=bad_candidates, boring_parameters=boring_parameters)
-np.load('data_center_region_small.npz')
+data = np.load('data_center_region_small.npz')
 
 print('\nNumber of bad candidates', len(bad_candidates), 'out of ', n_sample)
 print('Number of boring candidates', len(boring_parameters), 'out of ', n_sample)
 print('Number of saddles', len(parameter_full), 'out of ', n_sample)
-
 
 if bad_parameters is not None:
     fig1 = plt.figure()
@@ -72,7 +72,31 @@ if boring_parameters is not None:
     fig1 = plt.figure()
     dsgrn_plot(boring_parameters, 10)
     plt.title('No saddle detected')
+
+
+parameter_DSGRN = parameter_to_DSGRN_coord(parameter_full, 10)
+parameter_DSGRN = np.array([parameter_DSGRN[0], parameter_DSGRN[1]])
+unique_DSGRN = np.unique(parameter_DSGRN.round(decimals=5), axis=1)
+average_sol = np.empty(0)
+average_sol_long = 0*solutions
+for j in unique_DSGRN.T:
+    # work in progress
+    index_solution_j = np.where(abs(parameter_DSGRN[0, :] - j[0]) < 5*10**-5)
+    index_solution_loc = np.where(abs(parameter_DSGRN[1, :] - j[1]) < 5*10**-5)
+    index_solution_j = np.intersect1d(index_solution_j, index_solution_loc)
+    if len(index_solution_j)>0:
+        average_sol = np.append(average_sol, np.mean(solutions[index_solution_j]))
+        average_sol_long[index_solution_j] = np.mean(solutions[index_solution_j])
+    else:
+        print('wrong')
+
 fig1 = plt.figure()
-dsgrn_heat_plot(parameter_full, solutions, 10)
+dsgrn_heat_plot(parameter_full, average_sol_long, 10)
+
+fig1 = plt.figure()
+dsgrn_contour_plot(parameter_full, average_sol_long, 10)
+
+plt.figure()
+dsgrn_plot(parameter_full, 10)
 
 print('It is the end!')
