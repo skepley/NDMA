19511edc2b4b7af52466b4ccac10f02d201ebe38
Elena <elenaquei@hotmail.it>
2021-09-09 19:34:26 +0200

    corrected a 0 bound in radii pol.

--- hill_model.py
+++ hill_model.py
@@ -1384,6 +1384,8 @@ class HillModel:
             Y_bound = np.linalg.norm(A @ F(equilibrium))
             Z0_bound = np.linalg.norm(np.identity(len(equilibrium)) - A @ DF_x)
             Z2_bound = np.linalg.norm(A) * np.linalg.norm(D2F_x)
+            if Z2_bound<10^-16:
+                Z2_bound = 10^-10 # in case the Z2 bound is too close to zero, we increase it a bit
             delta = 1 - 4*(Z0_bound + Y_bound) * Z2_bound
             if delta<0:
                 return 0,0
