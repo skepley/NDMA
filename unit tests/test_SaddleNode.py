f19dcc76df7cdd1e1beed7302bdbcc93cd919c88
Elena <elenaquei@hotmail.it>
2023-08-08 13:04:15 +0200

    all unit tests green!
    but two tests "old" to fix when adjacency matrix constructor exists

--- unit tests/test_SaddleNode.py
+++ unit tests/test_SaddleNode.py
@@ -8,14 +8,19 @@ Function and design testing for the SaddleNode class
     Email: s.kepley@vu.nl
     Created: 4/13/2020 
 """
-from hill_model import SaddleNode
-from test_HillModel import ToggleSwitch
+from hill_model import *
+from models.TS_model import ToggleSwitch
+from saddle_node import SaddleNode
 
 
 # set some parameters to test using MATLAB toggle switch for ground truth
-decay = np.array([1, 1], dtype=float)
-p1 = np.array([1, 5, 3], dtype=float)
-p2 = np.array([1, 6, 3], dtype=float)
+#decay = np.array([1, 1], dtype=float)
+#p1 = np.array([1, 5, 3], dtype=float)
+#p2 = np.array([1, 6, 3], dtype=float)
+
+decay = np.array([np.nan, np.nan], dtype=float)
+p1 = np.array([np.nan, np.nan, np.nan], dtype=float)
+p2 = np.array([np.nan, np.nan, np.nan], dtype=float)
 x = np.array([4, 3])
 
 f = ToggleSwitch(decay, [p1, p2])
@@ -24,21 +29,29 @@ f2 = f.coordinates[1]
 H1 = f1.productionComponents[0]
 H2 = f2.productionComponents[0]
 n0 = 4.1
+par_start = 3.5
+p0 = np.array([1, 1, 5, 3, 1, 1, 6, 3])
 
-SN = SaddleNode(f, unit_phase_condition, diff_unit_phase_condition)
-eq = f.find_equilibria(n0, 10)
+SN = SaddleNode(f)
+eq = f.find_equilibria(3, 10, p0)
 # v0 = np.array([1, -.7])
-v0 = np.array([1, 1])
-x0 = eq[:, 1]
-u0 = np.concatenate((x0, v0, np.array(n0)), axis=None)
+# v0 = np.array([1, 1])
+x0 = eq[0, :]
+#u0 = np.concatenate((x0, v0, np.array(n0)), axis=None)
 
-uSol = find_root(SN.zero_map, SN.diff_zero_map, u0, diagnose=True)
-print(uSol)
-xSol, vSol, nSol = [uSol.x[idx] for idx in [[0, 1], [2, 3], [4]]]
+#uSol = find_root(SN.zero_map, SN.diff_zero_map, u0, diagnose=True)
+index_par = 0
+parSol = SN.find_saddle_node(index_par, par_start, p0, equilibria=x0)
+parSol = SN.find_saddle_node(index_par, par_start, p0)
+print(parSol)
 
 # plot nullclines
 plt.close('all')
 plt.figure()
-f.plot_nullcline(4.1)
+f.plot_nullcline(4.1, p0)
+plt.show()
 plt.figure()
-f.plot_nullcline(nSol)
+f.plot_nullcline(parSol, p0)
+plt.show()
+
+print(22)
