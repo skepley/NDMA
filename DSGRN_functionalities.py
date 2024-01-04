a3ca30bab8dcab5266480cf9d9bebdf0ed61a7dc
Elena <elenaquei@hotmail.it>
2024-01-04 14:25:57 +0100

    added point cloud creation functionality
    creates a cloud of points in a given DSGRN region

--- DSGRN_functionalities.py
+++ DSGRN_functionalities.py
@@ -1,6 +1,10 @@
+import warnings
+
 import DSGRN
 import numpy as np
 import json
+
+from hill_model import is_vector
 
 
 def from_string_to_Hill_data(DSGRN_par_string, network):
@@ -80,7 +84,7 @@ def HillContpar_to_DSGRN(par, indices_domain, indices_input, domain_size):
     OUTPUT
     L, T, U : arrays of ell, theta and u values
     """
-    data_size = int((len(par) - domain_size)/3)
+    data_size = int((len(par) - domain_size) / 3)
     gamma = par[0:domain_size]
     L = np.zeros((domain_size, domain_size))  # equation, input
     T = np.zeros((domain_size, domain_size))
@@ -99,7 +103,7 @@ def HillContpar_to_DSGRN(par, indices_domain, indices_input, domain_size):
     L[indices_domain, indices_input] = par[begin_L:end_L]
     T[indices_domain, indices_input] = par[begin_T:end_T]
     for i in range(np.shape(T)[0]):
-        T[i, :] = T[i, :]/gamma[i]
+        T[i, :] = T[i, :] / gamma[i]
     delta[indices_domain, indices_input] = par[begin_U:end_U]
     U = L + delta
     return L, U, T
@@ -131,8 +135,10 @@ def par_to_region_wrapper(regions_array, parameter_graph, indices_domain, indice
     def par_2_region(par_array):
         region_number = []
         for par in par_array.T:
-            region_number.append(par_to_region(par, regions_array, parameter_graph, indices_domain, indices_input, domain_size))
+            region_number.append(
+                par_to_region(par, regions_array, parameter_graph, indices_domain, indices_input, domain_size))
         return np.array(region_number)
+
     return par_2_region
 
 
@@ -185,8 +191,113 @@ def par_to_n_eqs(par, parameter_graph, indices_domain, indices_input, domain_siz
     """
     L, U, T = HillContpar_to_DSGRN(par, indices_domain, indices_input, domain_size)
     return_region_number = DSGRN.par_index_from_sample(parameter_graph, L, U, T)
-    if return_region_number<0:
+    if return_region_number < 0:
         print('Problem because region number cannot be negative - probably problem higher up')
     return DSGRN_FP_per_index(return_region_number, parameter_graph)
 
 
+def random_in_region(par_index, network, parameter_graph, size_out, variance=[]):
+    """
+    creates a random sample of parameters that all lie within the given DSGRN region index
+    INPUT
+    par_index : DSGRN region number
+    network, parameter_graph : DSGRN networks and parameter graphs
+    size_out : expected dimension of the output sample
+    variance (optional) : wished for variance used to generate the point cloud
+    OUTPUT
+    par_region : array of NDMA parameter values in the given region
+    """
+    domain_size = network.size()
+    parameternode = parameter_graph.parameter(par_index)
+    sampler = DSGRN.ParameterSampler(network)
+    p = sampler.sample(parameternode)
+    par_NDMA, indices_domain, indices_input = from_string_to_Hill_data(p, network)
+
+    def par_in_region(par):
+        return global_par_to_region(par, parameter_graph, indices_domain, indices_input, domain_size) == par_index
+
+    if not variance:
+        variance = 0.02 * np.eye(len(par_NDMA))
+    if is_vector(variance):
+        variance = np.diag(variance)
+
+    par_region = padded_filter_multivariate(par_NDMA, variance, size_out, lambda x: par_in_region(x))
+    return par_region
+
+
+def random_in_2regions(par_index1, par_index2, network, parameter_graph, size_out):
+    """
+    creates a random sample of parameters that all lie within the given DSGRN region index
+    INPUT
+    par_index1, par_index2 : DSGRN region number
+    network, parameter_graph : DSGRN networks and parameter graphs
+    size_out : expected dimension of the output sample
+    OUTPUT
+    par_region : array of NDMA parameter values in the two given regions
+    """
+    domain_size = network.size()
+    parameternode1 = parameter_graph.parameter(par_index1)
+    parameternode2 = parameter_graph.parameter(par_index2)
+    sampler = DSGRN.ParameterSampler(network)
+
+    p1 = sampler.sample(parameternode1)
+    p2 = sampler.sample(parameternode2)
+
+    par1, indices_domain, indices_input = from_string_to_Hill_data(p1, network)
+    par2, _, _ = from_string_to_Hill_data(p2, network)
+
+    mean = (par1 + par2) / 2
+    main_axes = (par1 - par2) / 2
+    distance_of_region_centers = np.linalg.norm(main_axes)
+    variance = 0.01 * np.eye(len(mean))
+    rescale_factor = distance_of_region_centers / mean[0]
+    variance[0, :] = rescale_factor * main_axes
+
+    def par_in_2regions(par):
+        index = global_par_to_region(par, parameter_graph, indices_domain, indices_input, domain_size)
+        return index == par_index1 or index == par_index2
+
+    par_region = padded_filter_multivariate(mean, variance, size_out, par_in_2regions)
+
+    return par_region
+
+
+def padded_filter_multivariate(mean, variance, size_out, lambda_function):
+    """
+    this function returns an array of vectors sampled from the multivariate normal distribution with given mean and
+    variance that satisfies the given boolean lambda function
+    INPUT
+    mean, variance : for the normal multivariate distribution
+    size_out : size of the filtered point cloud - if achieved
+    lambda_function : boolean function used for filtering
+    OUTPUT
+    point_cloud : point cloud satisfying the given boolean function
+    """
+    par_region = filtered_multivariate(mean, variance, size_out * 10, lambda_function)
+    iter_rand = 0
+    while np.size(par_region, 0) < size_out and iter_rand < 5:
+        iter_rand += 1
+        variance = variance * 0.95 # decrease variance to increase hit rate, hopefully
+        par_region = np.append(par_region, filtered_multivariate(mean, variance, size_out * 10, lambda_function), axis=0)
+    par_region = par_region[:size_out, :]
+    if np.size(par_region, 0) < size_out:
+        warnings.warn("default", print('Number of points created within the region in lower than expected\n',
+                                       'Number of expected points: ', size_out, '\nNumber of points: ',
+                                       len(par_region)))
+    return par_region
+
+
+def filtered_multivariate(mean, variance, size, lambda_function):
+    """
+    this function returns an array of vectors sampled from the multivariate normal distribution with given mean and
+    variance that satisfies the given boolean lambda function
+    INPUT
+    mean, variance : for the normal multivariate distribution
+    size : size of the point cloud (NOT of the final result)
+    lambda_function : boolean function used for filtering
+    OUTPUT
+    point_cloud : point cloud satisfying the given boolean function
+    """
+    random_cloud = np.abs(np.random.multivariate_normal(mean, variance, size)) # only the positive quadrant is considered
+    point_cloud = random_cloud[[lambda_function(vec) for vec in random_cloud]]
+    return point_cloud
